8ada8fbcbaf21bfcc45fddb551cfcc4d80f1cfa5
2203031260022 <157503251+2203031260022@users.noreply.github.com>
2024-01-24 00:14:18 +0530

    practical.1.py

--- practical.1.py
+++ practical.1.py
@@ -1,9 +1,10 @@
+fh=open("output1.txt",'w')
 # F = (9/5)C + 32  celsius to fahrenheit
 c=float(input("enter celsius value : "))
 f=(c*(9/5)+32)
-print("fahrenheit is : ",f)
+fh.write("fahrenheit is : " +str(f))
     
 #, °C = (°F - 32) × 5/9  fahrenheit to celsius
 f=int(input("enter fahrenheit value : "))
 c=((f-32)*5/9)
-print("celsius convertion is: ",c)
+fh.write("celsius convertion "+str(c))
